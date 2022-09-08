133591447978816c43126d7c2a21a82179bb6415
CodreanuML <102348157+CodreanuML@users.noreply.github.com>
2022-09-08 15:14:15 +0300

    Adding Queue with priority
    Altering Queue functionality . Check priority

--- Data Structures/Queue.py
+++ Data Structures/Queue.py
@@ -1,7 +1,8 @@
 class Node():
-    def __init__(self,value,next_node=None):
+    def __init__(self,value,next_node=None,weight=None):
         self.value=value
         self.next_node=next_node
+        self.weight=weight
         
     def __str__(self):
         return str(self.value)
@@ -11,6 +12,9 @@ class Node():
         
     def get_next_node(self):
         return self.next_node
+        
+    def get_weight(self,weight):
+        return self.weight
         
 
 class Queue():
@@ -78,6 +82,59 @@ class Queue():
             first=first.get_next_node()
 
 
+class Queue_with_priority(Queue):
+    def __init__(self,len):
+        super().__init__(len)
+    
+
+    
+    def traverse(self):
+        
+        first=self.head
+        while first!=None :
+            print("Nodul are valoare " + str(first.value)+" si greutatea :"+str(first.weight))
+            
+            first=first.get_next_node()
+    
+    
+    def enque(self,node):
+        
+        
+        if self.has_space():
+            
+            # check node weight
+            if self.head==None :
+                
+                self.tail=node
+                self.head=self.tail
+                self.actual_len+=1
+            else:
+                
+                #case 1 when node hase a bigger weight than head
+                if self.head.weight<node.weight:
+                    node.set_next_node(self.head)
+                    self.head=node
+                    self.actual_len+=1
+                #case 2 when you have to check every node
+                else:
+                
+                    first=self.head
+                
+                    
+                
+                    while first.get_next_node()!=None and first.get_next_node().weight>node.weight :
+                        first=first.get_next_node()
+                
+                    temp=first.get_next_node()
+                    node.set_next_node(temp)
+                    first.set_next_node(node)
+                
+                
+                    self.actual_len+=1            
+ 
+        else :
+            print( "Queue is out of space: max len is %d  %s "   %(self.actual_len,"please dequeue"  ))
+    
     
 ## Nodes Definition
 First=Node(1)
@@ -88,7 +145,10 @@ Fifth=Node(5)
 Sixth=Node(6)
 Seventh=Node(7)
 
-#Queue Definition
+#Queue Definition -Non prio FIFO
+
+print("#################Non Prio########################")
+
 New_Queue=Queue(5)
 print(New_Queue.is_empty())
 New_Queue.enque(First)
@@ -105,3 +165,26 @@ print(New_Queue.traverse())
 New_Queue.enque(Sixth)
 New_Queue.left_queue(Second)
 print(New_Queue.traverse())
+
+
+#Queue Definition -Prio - altering  FIFO (in functie de importanta fiecarui nod)
+print("################# Prio ########################")
+
+
+First_P=Node(value=1,weight=243)
+Second_P=Node(value=2,weight=244)
+Third_P=Node(value=3,weight=240)
+Fourth_P=Node(value=4,weight=242)
+Fifth_P=Node(value=5,weight=250)
+Sixth_P=Node(value=6,weight=241)
+New_Queue_p=Queue_with_priority(5)
+print(New_Queue_p.is_empty())
+New_Queue_p.enque(First_P)
+New_Queue_p.enque(Second_P)
+New_Queue_p.enque(Third_P)
+New_Queue_p.enque(Fourth_P)
+New_Queue_p.enque(Fifth_P)
+New_Queue_p.deque()
+New_Queue_p.enque(Sixth_P)
+
+print(New_Queue_p.traverse())
