48fc4d00c4369f3c1cfcb506ecbac7b52dd40302
CodreanuML <102348157+CodreanuML@users.noreply.github.com>
2022-09-13 16:13:12 +0300

    Update - collision avoiding
    Adding collision fixing using linked list to store chained nodes

--- Data Structures/HashMap.py
+++ Data Structures/HashMap.py
@@ -68,3 +68,101 @@ for i in Records:
 print(MyHash)
 MyHash.retrieve_record("Cal")
 MyHash.retrieve_record("Lucian")
+
+#############################ADDING A LINK LIST CLASS################################
+########################FIRST STEP TO AVOID COLLISIONS###############################
+
+
+##CREATING A CHAINED HASH MAP USING LINKED LIST
+##IMPORTING LINKED LISTE STRUCTURE
+
+class Node():
+
+
+	def __init__(self,value):
+		self.value=value
+		self.next_node=None
+
+	def add_next_node(self,node):
+		self.next_node=node
+
+	def get_next_node(self):
+		return self.next_node
+
+	def __rpr__(self):
+		return self.value
+
+
+
+class Linked_List():
+
+	def __init__(self):
+		self.head=None
+
+	def add(self,Node):
+		if self.head==None:
+
+			self.head=Node
+
+		else:
+
+			temp=self.head
+			self.head=Node
+			self.head.add_next_node(temp)
+
+
+	def traverse(self):
+		node_f=self.head
+		while node_f :
+			print(node_f.value)
+			node_f=node_f.get_next_node()
+			
+	
+	def getnode(self,node):
+		node_f=self.head
+		while node_f :
+			if node_f.value[0]==node:
+			    return node_f.value
+			node_f=node_f.get_next_node()
+
+
+class HashMap_collision_linked(HashMap):
+    def __init__(self,len):
+        self.len=len
+        self.hash_array=[]
+        for i in range(0,len):
+            self.hash_array.append(Linked_List())        
+    
+    def __str__(self):
+        return str(self.hash_array)       
+        
+        
+    def add_record(self,input):
+        hashed_key=0
+        hashed_key=self.hashing(input[0])
+        
+        list=self.hash_array[hashed_key]
+        
+        list.add(Node(input))
+        
+        
+    def get_node_value(self,input):
+        hashed_key=0
+        hashed_key=self.hashing(input)
+        lista=self.hash_array[hashed_key]
+        allowed=lista.getnode(input)
+        
+        
+        if allowed !=None :
+            print("input gasit  gasit la index {}".format(hashed_key))
+            print (str(input)+" are numarul " + str(allowed[1]))
+        else:
+            print ("Node not found")
+            
+Hash_map_linked=HashMap_collision_linked(5)
+print(Hash_map_linked)
+for i in Records:
+    Hash_map_linked.add_record(i)
+
+Hash_map_linked.hash_array[1].traverse()
+Hash_map_linked.get_node_value('Ganny')
